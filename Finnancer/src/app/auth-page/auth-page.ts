f5b84eef927a068aed94e49d4dcf6f95ec756a81
LewisAngelo11 <luisangel.sotog11@gmail.com>
2025-09-19 12:07:12 -0700

    Principales cambios al login y al sign in (Solo en Móviles)

---
+++ Finnancer/src/app/auth-page/auth-page.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { Login } from '../login/login';
+import { Register } from '../register/register';
+
+@Component({
+  selector: 'app-auth-page',
+  imports: [Login, Register],
+  templateUrl: './auth-page.html',
+  styleUrl: './auth-page.css'
+})
+export class AuthPage {
+  buttonSelected = 'login';
+
+  changeModal(modal: string) {
+    switch(modal) {
+      case 'login':
+        this.buttonSelected = 'login';
+        break;
+      case 'register':
+        this.buttonSelected = 'register';
+        break;
+    }
+  }
+}
