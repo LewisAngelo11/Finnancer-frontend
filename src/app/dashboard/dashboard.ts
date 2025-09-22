0b6918b6818a4076c904bb9d4be88e3e328788a5
LewisAngelo11 <luisangel.sotog11@gmail.com>
2025-09-22 11:29:30 -0700

    Avance de la interfaz del dashboard (solo en móviles)

--- src/app/dashboard/dashboard.ts
+++ src/app/dashboard/dashboard.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { HeaderDashboard } from '../header-dashboard/header-dashboard';
 
 @Component({
@@ -9,4 +9,29 @@ import { HeaderDashboard } from '../header-dashboard/header-dashboard';
 })
 export class Dashboard {
   presupuesto: number = 200500;
+  ingresosMensuales: number = 10000;
+  ingresosMinimos: number = 4000;
+  egresosMensuales: number = 4000;
+  egresosMinimos: number = 3000;
+
+  isPositive = signal(true);
+  signo: string = '';
+
+  calcularIngresos(ingresosMen:number, ingresosMin:number) {
+    return ingresosMen - ingresosMin;
+  }
+
+  calcularEgresos(egresosMen: number, egresosMin: number) {
+    return egresosMen - egresosMin;
+  }
+
+  verificarPositivo() {
+    let result = this.calcularIngresos(this.ingresosMensuales, this.ingresosMinimos)
+
+    if (result > 0) {
+      this.isPositive.set(true);
+      this.signo = '+';
+    }
+  }
+
 }
