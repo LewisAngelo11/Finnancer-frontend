a0acfc0e2529d83c9fc9160033910f968c8cbda0
LewisAngelo11 <luisangel.sotog11@gmail.com>
2025-11-22 21:58:06 -0700

    Cambios en la interfaz de el módulo de las categorías.

--- src/app/categories/subcategories-table/subcategories-table.ts
+++ src/app/categories/subcategories-table/subcategories-table.ts
@@ -19,6 +19,24 @@ export class SubcategoriesTable implements OnInit {
 
   selectedFilterSubcategories = signal<FiltrosSubcategorias>('Todos');
 
+  // Iconos para las categorías y subcategorías
+  icons: Record<number, string> = {
+    1: 'bx bx-money',
+    2: 'bx bx-bulb',
+    3: 'bx bx-trending-up',
+    4: 'bx bx-trending-down',
+    5: 'bx bx-water',
+    6: 'bx bx-money',
+    7: 'bx bx-receipt',
+    8: 'bx bx-dollar',
+    9: 'bx bx-building',
+    10: 'bx bx-coin',
+    11: 'bx bx-line-chart',
+    12: 'bx bx-bookmark',
+    13: 'bx-bar-chart-alt-2',
+    14: 'bx bx-credit-card',
+  };
+
   ngOnInit(): void {
     this.subcategoriaService.getAllSubcategories().subscribe({
       next: (res) => {
